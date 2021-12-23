010af5d2c6d34654c91bc89f8428c228e64c6f63
Şafak Temel <safak.temel@birlesikodeme.com>
2021-12-23 23:18:41 +0300

    add layz loading for dummy module

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'order',
+    loadChildren: () => import('./order/order.module').then(m => m.OrderModule),
+  },  
+  {
+    path: 'order-statu',
+    loadChildren: () => import('./order-statu/order-statu.module').then(m => m.OrderStatuModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
